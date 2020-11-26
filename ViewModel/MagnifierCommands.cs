6b554b5de231b2e80dc918b34b735f6cdd2d92fd
Adina Pepene <adinaflorentina_2000@yahoo.com>
2020-11-26 15:04:38 +0200

    Implemented magnifier for gray images.

--- ViewModel/MagnifierCommands.cs
+++ ViewModel/MagnifierCommands.cs
@@ -4,6 +4,7 @@ using OxyPlot;
 using System;
 using OxyPlot.Axes;
 using OxyPlot.Series;
+using ImageProcessingFramework.Model;
 
 namespace ImageProcessingFramework.ViewModel
 {
@@ -12,54 +13,71 @@ namespace ImageProcessingFramework.ViewModel
 
         public PlotModel MagnifierColorImage(Image<Bgr, byte> colorImage)
         {
-            plotImage = new PlotModel();
-            plotImage.Axes.Add(new CategoryAxis
-            {
-                Position = AxisPosition.Bottom,
-                Key = "xAxis",
-                ItemsSource = new[] {
-                                    "1",
-                                    "2",
-                                    "3",
-                                    "4",
-                                    "5",
-                                    "6",
-                                    "7",
-                                    "8",
-                                    "9"
-                }
-            });
-
-            plotImage.Axes.Add(new CategoryAxis
-            {
-                Position = AxisPosition.Left,
-                Key = "yAxis",
-                ItemsSource = new[] {
-                                    "1",
-                                    "2",
-                                    "3",
-                                    "4",
-                                    "5",
-                                    "6",
-                                    "7",
-                                    "8",
-                                    "9"
-                }
-            });
+            CreateAxisForPlot();
 
             plotImage.Axes.Add(new LinearColorAxis
             {
-                Palette = OxyPalettes.Hot(200)
+                Palette = OxyPalettes.Hot(255)
             });
-            var rand = new Random();
-            var data = new double[9, 9];
-            for (int x = 0; x < 9; ++x)
+
+            var dataGreen = GenerateColorPixel(colorImage, 0);
+            var dataRed = GenerateColorPixel(colorImage, 1);
+            var dataBlue = GenerateColorPixel(colorImage, 2);
+
+            var heatMapSeriesBlue = new HeatMapSeries
             {
-                for (int y = 0; y < 9; ++y)
-                {
-                    data[y, x] = rand.Next(0, 200) * (0.13 * (y + 1));
-                }
-            }
+                X0 = 0,
+                X1 = 8,
+                Y0 = 0,
+                Y1 = 8,
+                XAxisKey = "xAxis",
+                YAxisKey = "yAxis",
+                RenderMethod = HeatMapRenderMethod.Rectangles,
+                LabelFontSize = 0.2,
+                Data = dataBlue
+            };
+            var heatMapSeriesGreen = new HeatMapSeries
+            {
+                X0 = 0,
+                X1 = 8,
+                Y0 = 0,
+                Y1 = 8,
+                XAxisKey = "xAxis",
+                YAxisKey = "yAxis",
+                RenderMethod = HeatMapRenderMethod.Rectangles,
+                LabelFontSize = 0.2,
+                Data = dataGreen
+            };
+            var heatMapSeriesRed = new HeatMapSeries
+            {
+                X0 = 0,
+                X1 = 8,
+                Y0 = 0,
+                Y1 = 8,
+                XAxisKey = "xAxis",
+                YAxisKey = "yAxis",
+                RenderMethod = HeatMapRenderMethod.Rectangles,
+                LabelFontSize = 0.2,
+                Data = dataRed
+            };
+            plotImage.Series.Add(heatMapSeriesBlue);
+            plotImage.Series.Add(heatMapSeriesGreen);
+            plotImage.Series.Add(heatMapSeriesRed);
+
+            return plotImage;
+        }
+
+        public PlotModel MagnifierGrayImage(Image<Gray, byte> grayImage)
+        {
+            CreateAxisForPlot();
+
+            plotImage.Axes.Add(new LinearColorAxis
+            {
+                Palette = OxyPalettes.Gray(255)
+            });
+
+            var data = GenerateGrayPixel(grayImage);
+            
             var heatMapSeries = new HeatMapSeries
             {
                 X0 = 0,
@@ -77,7 +95,7 @@ namespace ImageProcessingFramework.ViewModel
             return plotImage;
         }
 
-        public PlotModel MagnifierGrayImage(Image<Gray, byte> grayImage)
+        public void CreateAxisForPlot()
         {
             plotImage = new PlotModel();
             plotImage.Axes.Add(new CategoryAxis
@@ -113,35 +131,46 @@ namespace ImageProcessingFramework.ViewModel
                                     "9"
                 }
             });
+        }
 
-            plotImage.Axes.Add(new LinearColorAxis
-            {
-                Palette = OxyPalettes.Hot(200)
-            });
-            var rand = new Random();
+        private double[,] GenerateColorPixel(Image<Bgr, byte> colorImage, int channel)
+        {
             var data = new double[9, 9];
-            for (int x = 0; x < 9; ++x)
+            int i = 0;
+            for (int x = -4; x < 5; ++x)
             {
-                for (int y = 0; y < 9; ++y)
+                int j = 0;
+                for (int y = -4; y < 5; ++y)
                 {
-                    data[y, x] = rand.Next(0, 200) * (0.13 * (y + 1));
+                    data[j, i] = colorImage.Data[(int)DataProvider.MousePosition.Y + y,
+                        (int)DataProvider.MousePosition.X + x,
+                        channel];
+                    ++j;
                 }
+                ++i;
             }
-            var heatMapSeries = new HeatMapSeries
-            {
-                X0 = 0,
-                X1 = 8,
-                Y0 = 0,
-                Y1 = 8,
-                XAxisKey = "xAxis",
-                YAxisKey = "yAxis",
-                RenderMethod = HeatMapRenderMethod.Rectangles,
-                LabelFontSize = 0.2,
-                Data = data
-            };
-            plotImage.Series.Add(heatMapSeries);
 
-            return plotImage;
+            return data;
+        }
+
+        private double[,] GenerateGrayPixel(Image<Gray, byte> grayImage)
+        {
+            var data = new double[9, 9];
+            int i = 0;
+            for (int x = -4; x <= 4; ++x)
+            {
+                int j = 0;
+                for (int y = 4; y >= -4; --y)
+                {
+                    data[i, j] = grayImage.Data[(int)DataProvider.MousePosition.Y + y,
+                        (int)DataProvider.MousePosition.X + x,
+                        0];
+                    ++j;
+                }
+                ++i;
+            }
+
+            return data;
         }
 
         public PlotModel plotImage { get; private set; }
